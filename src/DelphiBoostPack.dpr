f2e3d2ccaf5eb963917a6b84c7e78b1e82da9635
Dimitar Grigorov <dimitar-grigorov@users.noreply.github.com>
2024-04-04 12:02:24 +0300

    TBpIntList: Added tests to check for memory leaks. Not very good reporting, but they work.

--- src/DelphiBoostPack.dpr
+++ src/DelphiBoostPack.dpr
@@ -8,11 +8,9 @@ uses
   BpIntList in 'Core\Classes\BpIntList.pas',
   BpIntListInterface in 'Core\Interfaces\BpIntListInterface.pas';
 
-{var
-  sl: TStringList;}
 begin
+  System.ReportMemoryLeaksOnShutdown := True;
 
-
-
+  Readln;
 end.
 
