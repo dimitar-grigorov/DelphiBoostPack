fa0a6e757b01f7e45d96d3c842344dd9a7412ed9
Dimitar Grigorov <dimitar-grigorov@users.noreply.github.com>
2024-04-04 10:15:51 +0300

    TBpIntList: Now implements IBpIntList. Added methods IndexOf and Insert.

--- src/Core/Interfaces/BpIntListInterface.pas
+++ src/Core/Interfaces/BpIntListInterface.pas
@@ -4,15 +4,16 @@ interface
 
 type
   IBpIntList = interface
-    function Get(Index: Integer): Integer;
-    procedure Put(Index: Integer; const Item: Integer);
+    function GetItem(Index: Integer): Integer;
+    procedure SetItem(Index: Integer; const Value: Integer);
+    
     function GetCount: Integer;
     function Add(const Item: Integer): Integer;
-    procedure Delete(Index: Integer);
+    procedure Delete(const Index: Integer);
     procedure Clear;
     function IndexOf(const Item: Integer): Integer;
     procedure Insert(Index: Integer; const Item: Integer);
-    property Items[Index: Integer]: Integer read Get write Put; default;
+    property Items[Index: Integer]: Integer read GetItem write SetItem; default;
     property Count: Integer read GetCount;
   end;
 
