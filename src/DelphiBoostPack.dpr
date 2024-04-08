b7a06220499ffdbaf997daddce7b1b156f14cc4a
Dimitar Grigorov <mitak.backtrack@gmail.com>
2024-04-09 02:12:47 +0300

    Added TBpObjectComparer.

--- src/DelphiBoostPack.dpr
+++ src/DelphiBoostPack.dpr
@@ -6,7 +6,8 @@ uses
   SysUtils,
   Classes,
   BpIntList in 'Core\Classes\BpIntList.pas',
-  BpIntListInterface in 'Core\Interfaces\BpIntListInterface.pas';
+  BpIntListInterface in 'Core\Interfaces\BpIntListInterface.pas',
+  BpObjectComparer in 'Core\Classes\BpObjectComparer.pas';
 
 begin
   System.ReportMemoryLeaksOnShutdown := True;
