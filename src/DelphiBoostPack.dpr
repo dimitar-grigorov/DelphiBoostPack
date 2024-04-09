a280b8d7d8e5e72d4595632afef3556978c471b0
Dimitar Grigorov <mitak.backtrack@gmail.com>
2024-04-09 22:45:00 +0300

    Added TInterfacedCollectionItem which adds to its descendants to support/implement interfaces. It is descendant of TCollectionItem.

--- src/DelphiBoostPack.dpr
+++ src/DelphiBoostPack.dpr
@@ -8,7 +8,8 @@ uses
   BpIntListUnit in 'Core\Classes\BpIntListUnit.pas',
   IBpIntListUnit in 'Core\Interfaces\IBpIntListUnit.pas',
   BpObjectComparerUnit in 'Core\Classes\BpObjectComparerUnit.pas',
-  IUniqueIdUnit in 'Core\Interfaces\IUniqueIdUnit.pas';
+  IUniqueIdUnit in 'Core\Interfaces\IUniqueIdUnit.pas',
+  InterfacedCollectionItemUnit in 'Core\Classes\InterfacedCollectionItemUnit.pas';
 
 begin
   System.ReportMemoryLeaksOnShutdown := True;
