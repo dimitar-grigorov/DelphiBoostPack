8121f8a0ea47057fd2abe27a6d81f13251d0fc4b
Dimitar Grigorov <dimitar-grigorov@users.noreply.github.com>
2024-04-11 02:50:46 +0300

    Added implementation of HashBobJenkins algorithm.

--- src/DelphiBoostPack.dpr
+++ src/DelphiBoostPack.dpr
@@ -9,7 +9,8 @@ uses
   IBpIntListUnit in 'Core\Interfaces\IBpIntListUnit.pas',
   BpObjectComparerUnit in 'Core\Classes\BpObjectComparerUnit.pas',
   IUniqueIdUnit in 'Core\Interfaces\IUniqueIdUnit.pas',
-  InterfacedCollectionItemUnit in 'Core\Classes\InterfacedCollectionItemUnit.pas';
+  InterfacedCollectionItemUnit in 'Core\Classes\InterfacedCollectionItemUnit.pas',
+  BpHashBobJenkinsUnit in 'Core\Classes\BpHashBobJenkinsUnit.pas';
 
 begin
   System.ReportMemoryLeaksOnShutdown := True;
