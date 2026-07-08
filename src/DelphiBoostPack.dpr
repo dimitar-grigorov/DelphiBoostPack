18ba0010cb708e0bc11b4df9ef8a2cb81d017a2e
Dimitar Grigorov <dimitar-grigorov@users.noreply.github.com>
2026-07-08 06:11:36 +0300

    Add TbpHttpClient, WinInet HTTP client with bearer and basic auth.

--- src/DelphiBoostPack.dpr
+++ src/DelphiBoostPack.dpr
@@ -14,6 +14,7 @@ uses
   BpStrDictionary in 'Core\Classes\BpStrDictionary.pas',
   BpStringBuilder in 'Core\Classes\BpStringBuilder.pas',
   BpHMACSHA256 in 'Core\Classes\BpHMACSHA256.pas',
+  BpHttpClient in 'Core\Classes\BpHttpClient.pas',
   BpMD5 in 'Core\Classes\BpMD5.pas',
   BpSHA256 in 'Core\Classes\BpSHA256.pas',
   BpBase64 in 'Core\Units\BpBase64.pas',
